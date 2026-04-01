114b812744b1cc0e593858655918f3c57be72f23
Arseniynovikov <arseniydfg@gmail.com>
2026-04-01 19:19:07 +0300

    Update of Repository.kt and RepositoryImpl.kt

--- app/src/main/java/com/example/pexelpinterest/data/repository/Repository.kt
+++ app/src/main/java/com/example/pexelpinterest/data/repository/Repository.kt
@@ -3,18 +3,19 @@ package com.example.pexelpinterest.data.repository
 import com.example.pexelpinterest.data.database.PhotoEntity
 import com.example.pexelpinterest.data.retrofit.models.PhotoApi
 import com.example.pexelpinterest.data.retrofit.models.PhotoResponse
+import com.example.pexelpinterest.domain.Photo
 
 import kotlinx.coroutines.flow.Flow
 
 
 interface Repository {
 
-    suspend fun insertBookmarkPhoto(photoEntity: PhotoEntity)
-    suspend fun deleteBookmarkPhoto(photoEntity: PhotoEntity)
-    fun getAllBookmarkPhotos(): Flow<List<PhotoEntity>>
-    fun getBookmarkPhotoById(id: Int): Flow<PhotoEntity>
+    suspend fun insertBookmarkPhoto(photo: Photo)
+    suspend fun deleteBookmarkPhoto(photo: Photo)
+    fun getAllBookmarkPhotos(): Flow<List<Photo>>
+    fun getBookmarkPhotoById(id: Int): Flow<Photo>
 
-    suspend fun getCuratedPhotos(page: Int, perPage: Int): PhotoResponse
-    suspend fun searchPhotos(query: String, page: Int, perPage: Int): PhotoResponse
-    suspend fun getPhotoById(id: Long): PhotoApi
+    suspend fun getCuratedPhotos(page: Int, perPage: Int): List<Photo>
+    suspend fun searchPhotos(query: String, page: Int, perPage: Int): List<Photo>
+    suspend fun getPhotoById(id: Long): Photo
 }
--- app/src/main/java/com/example/pexelpinterest/domain/RepositoryImpl.kt
+++ app/src/main/java/com/example/pexelpinterest/domain/RepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.pexelpinterest.domain
 
 import com.example.pexelpinterest.data.database.AppDatabase
+import com.example.pexelpinterest.data.database.Dao
 import com.example.pexelpinterest.data.database.PhotoEntity
 import com.example.pexelpinterest.data.repository.Repository
 import com.example.pexelpinterest.data.retrofit.PexelApi
@@ -8,44 +9,42 @@ import com.example.pexelpinterest.data.retrofit.PexelApi
 import com.example.pexelpinterest.data.retrofit.models.PhotoApi
 import com.example.pexelpinterest.data.retrofit.models.PhotoResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
-    val database: AppDatabase,
+    val dao: Dao,
     val pexelApi: PexelApi
 ): Repository {
-    override suspend fun insertBookmarkPhoto(photoEntity: PhotoEntity) {
-        TODO("Not yet implemented")
+    override suspend fun insertBookmarkPhoto(photo: Photo) {
+        dao.insertBookmarkPhoto(photo.toEntity())
     }
 
-    override suspend fun deleteBookmarkPhoto(photoEntity: PhotoEntity) {
-        TODO("Not yet implemented")
+    override suspend fun deleteBookmarkPhoto(photo: Photo) {
+        dao.deleteBookmarkPhoto(photo.toEntity())
     }
 
-    override fun getAllBookmarkPhotos(): Flow<List<PhotoEntity>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAllBookmarkPhotos(): Flow<List<Photo>> =
+        dao.getAllBookmarkPhotos().map { it -> it.map { photoEntity -> photoEntity.toDomain() } }
 
-    override fun getBookmarkPhotoById(id: Int): Flow<PhotoEntity> {
-        TODO("Not yet implemented")
-    }
+    override fun getBookmarkPhotoById(id: Int): Flow<Photo> =
+        dao.getBookmarkPhotoById(id).map { photoEntity -> photoEntity.toDomain() }
 
     override suspend fun getCuratedPhotos(
         page: Int,
         perPage: Int
-    ): PhotoResponse {
-        TODO("Not yet implemented")
-    }
+    ): List<Photo> =
+        pexelApi.getCuratedPhotos(page, perPage).photoApis.map { it-> it.toDomain() }
+
 
     override suspend fun searchPhotos(
         query: String,
         page: Int,
         perPage: Int
-    ): PhotoResponse {
-        TODO("Not yet implemented")
-    }
+    ): List<Photo> =
+        pexelApi.searchPhotos(query, page, perPage).photoApis.map{it -> it.toDomain()}
 
-    override suspend fun getPhotoById(id: Long): PhotoApi {
-        TODO("Not yet implemented")
-    }
+
+    override suspend fun getPhotoById(id: Long): Photo =
+        pexelApi.getPhotoById(id).toDomain()
 }
